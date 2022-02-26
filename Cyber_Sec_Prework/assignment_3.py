9c8f34cca455a8a81556ef69115276daa0c9b20d
AyOsir <32373129+AyOsir@users.noreply.github.com>
2022-02-25 23:26:58 -0500

    Create assignment_3.py

---
+++ Cyber_Sec_Prework/assignment_3.py
@@ -0,0 +1,12 @@
+#the list
+over_under_list = [1,45,32,21,5,17,43,93]
+
+#traverse all numbers in the list
+for num in over_under_list:
+    #check if number greater than 25
+    if num>25:
+        #print "over"
+        print("over")
+    #check if number smaller than 25
+    if num<25:
+        print("under")
